f8ecf3d6c2dafb8c6fe57bce63e59615356099a4
S04Nik <dedmazai.papai@gmail.com>
2020-10-16 16:36:46 +0300

    docker app deleted files - ZSTU

--- auth/api/helpers/helper.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const {db}=require('../configuration/index');
-
-module.exports.connectDB=()=>{
-    console.log(db);
-    mongoose.connect(db,{useNewUrlParser:true})
-    return mongoose.connection
-}
